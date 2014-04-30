a19ad69ad42d41eb3ce081a6e69c6e5966028d23
Vladimir Vesely <ivesely@fit.vutbr.cz>
2014-04-30 12:02:28 -0700

    Introducing RIB

---
+++ src/DIF/RIB/LocalDirectoryCache.cc
@@ -0,0 +1,35 @@
+//
+// Copyright © 2014 PRISTINE Consortium (http://ict-pristine.eu)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+/**
+ * @file LocalDirectoryCache.cc
+ * @author Vladimir Vesely (ivesely@fit.vutbr.cz)
+ * @date Apr 30, 2014
+ * @brief
+ * @detail
+ */
+
+#include <RIB/LocalDirectoryCache.h>
+
+LocalDirectoryCache::LocalDirectoryCache() {
+    // TODO Auto-generated constructor stub
+
+}
+
+LocalDirectoryCache::~LocalDirectoryCache() {
+    // TODO Auto-generated destructor stub
+}
+
