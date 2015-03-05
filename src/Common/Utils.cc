ed47c761138fb430849f022203a588b6e4306461
gaixas1 <gaixas1@gmail.com>
2015-03-05 12:58:32 +0100

    Added Common/Utils file, for storing common functions usable on policies,etc., like string modifiers.

---
+++ src/Common/Utils.cc
@@ -0,0 +1,23 @@
+/*
+ * Utils.cpp
+ *
+ *  Created on: Mar 5, 2015
+ *      Author: gaixas1
+ */
+
+#include <Utils.h>
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+   while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
---
+++ src/Common/Utils.h
@@ -0,0 +1,15 @@
+/*
+ * Utils.h
+ *
+ *  Created on: Mar 5, 2015
+ *      Author: gaixas1
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems);
+std::vector<std::string> split(const std::string &s, char delim);
+
+
+#endif /* UTILS_H_ */
