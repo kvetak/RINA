db394fa1d52c9bcd4e9238c34864906c6d6b2429
Vladimir Vesely <ivesely@fit.vutbr.cz>
2015-06-30 14:47:15 +0200

    :book: Updating SLOC script :bar_chart:

--- scripts/slocstats.sh
+++ scripts/slocstats.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "This script counts source lines of codes of RINASim project"
+echo "This script counts SLOCs of RINASim project"
 echo "C++ code (*.cc, *.h)"
 git ls-files .. | grep -E '*.cc|*.h'| xargs cat | wc -l
 echo "OMNeT++ code (*.msg, *.ned)"
 git ls-files .. | grep -E '*.msg|*.ned'| xargs cat | wc -l
 echo "Simulation code (*.ini, *.xml)"
 git ls-files .. | grep -E '*.ini|*.xml'| xargs cat | wc -l
-echo "Project code (*.cc, *.h, *.msg, *.ned, *.ini, *.xml)"
+echo "Whole project code (*.cc, *.h, *.msg, *.ned, *.ini, *.xml)"
 git ls-files .. | grep -E '*.cc|*.h|*.msg|*.ned|*.xml|*.ini'| xargs cat | wc -l
-echo "Files watched by GIT"
+echo "All files watched by GIT"
 git ls-files .. | xargs cat | wc -l 
