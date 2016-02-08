79d8a6c16b549bb22e7310f933f2d5b434636eb6
Vladimir Vesely <ivesely@fit.vutbr.cz>
2016-02-08 13:37:21 +0100

    :birthday: 1000th commit referential release :beers: :tada:
    Referential release with all the neat features like fragmentation or DAF
    enrollment

--- scripts/slocstats.sh
+++ scripts/slocstats.sh
@@ -5,18 +5,18 @@ printf "language   extension\tall\texcl.comments\texcl.pattern\n"
 printf "===============================================================\n"
 
 #C++
-cppall=`git ls-files .. | grep -E '*.cc|*.h' | xargs cat | wc -l`
-cppexc=`git ls-files .. | grep -E '*.cc|*.h' | xargs cat | sed '/\/\//d' | sed '/^\s*$/d' | sed '/\/\*/,/*\//d' | wc -l`
+cppall=`git ls-files .. | grep -E '\.cc|\.h' | xargs cat | wc -l`
+cppexc=`git ls-files .. | grep -E '\.cc|\.h' | xargs cat | sed '/\/\//d' | sed '/^\s*$/d' | sed '/\/\*/,/*\//d' | wc -l`
 printf "C++        *.cc  *.h\t$cppall\t$cppexc\t\tempty // /**/\n"
 
 #OMNeT
-omnall=`git ls-files .. | grep -E '*.msg|*.ned' | xargs cat | wc -l`
-omnexc=`git ls-files .. | grep -E '*.msg|*.ned' | xargs cat | sed '/\/\//d' | sed '/^\s*$/d' | wc -l`
+omnall=`git ls-files .. | grep -E '\.msg|\.ned' | xargs cat | wc -l`
+omnexc=`git ls-files .. | grep -E '\.msg|\.ned' | xargs cat | sed '/\/\//d' | sed '/^\s*$/d' | wc -l`
 printf "NED        *.msg *.ned\t$omnall\t$omnexc\t\tempty //\n"
 
 #Simul
-simall=`git ls-files .. | grep -E '*.ini|*.xml' | xargs cat 2>/dev/null | wc -l`
-simexc=`git ls-files .. | grep -E '*.ini|*.xml' | xargs cat 2>/dev/null | sed '/\s*#.*$/d' | sed '/^\s*$/d' | wc -l`
+simall=`git ls-files .. | grep -E '\.ini|\.xml' | xargs cat 2>/dev/null | wc -l`
+simexc=`git ls-files .. | grep -E '\.ini|\.xml' | xargs cat 2>/dev/null | sed '/\s*#.*$/d' | sed '/^\s*$/d' | wc -l`
 printf "OMNeT++    *.ini *.xml\t$simall\t$simexc\t\tempty #\n"
 printf "===============================================================\n"
 
