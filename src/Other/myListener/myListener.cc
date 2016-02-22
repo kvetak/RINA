35695bb20348d44f5f8d225a855d8286b6e10549
eelahi <eelahi@192.168.192.3>
2016-02-22 15:35:22 +0000

    Right place for Signal to be searched

--- src/Other/myListener/myListener.cc
+++ src/Other/myListener/myListener.cc
@@ -61,5 +61,6 @@ void myListener::handleMessage(cMessage *msg) {
 
 void myListener::receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj) {
     Enter_Method_Silent();
-    std::cout<< "Signal Received: Source:" << source->getFullPath() << ", MSG:" << dynamic_cast<std::string*>(obj) << std::endl;
+    std::string *str = dynamic_cast<std::string*>(obj);
+    std::cout<< "Signal Received: Source:" << source->getFullPath() << ", MSG:" << str << std::endl;
 }
